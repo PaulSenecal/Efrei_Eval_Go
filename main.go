fa833af380c7ceeab58557794be256ddcd204347
paulz <senecalpaul@outlook.fr>
2025-06-23 12:13:40 +0200

    fix: fex bug

--- main.go
+++ main.go
@@ -2,9 +2,9 @@
 package main
 
 import (
-	_ "github.com/axellelanca/urlshortener/cmd/cli"    // Importe le package 'cli' pour que ses init() soient exécutés
-	_ "github.com/axellelanca/urlshortener/cmd/server" // Importe le package 'server' pour que ses init() soient exécutés
-	"url-shortener/cmd"
+	_ "./cmd/cli"    // Importe le package 'cli' pour que ses init() soient exécutés
+	_ "./cmd/server" // Importe le package 'server' pour que ses init() soient exécutés
+	"./cmd"
 )
 
 func main() {
